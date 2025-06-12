b55b4e87bde7839402a6cc28b9aafba8c11b9456
alesalo03 <alessandrosalomoni56@gmail.com>
2025-06-12 12:50:29 +0200

    prima versione dello script python

--- VSCode/artifacts_verification_script.py
+++ VSCode/artifacts_verification_script.py
@@ -3,9 +3,27 @@ import subprocess
 import sys
 
 
-def verify_files(folder):
-    #Defiinisco una variabile che a fine ciclo indicherà se la verifica della firma è andata a buon fine PER OGNUNO dei file esaminati
+def verify_files(folder, publickey):
+    #Definisco una variabile che a fine ciclo indicherà se la verifica della firma è andata a buon fine PER OGNUNO dei file esaminati
     all_signatures_valid = True
+    number_of_signatures_verified = 0
+
+    #Importo la chiave all'interno del keyring
+    try:
+        print(f"Importing public key: {publickey}")
+        subprocess.run(
+            ["gpg", "--import", publickey],
+            check=True,
+            capture_output=True,
+            text=True
+        )
+        print("Public key imported successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to import public key: {e}")
+        print(f"Command output: {e.stdout}")
+        print(f"Command error: {e.stderr}")
+        return False
+
 
     # Scansiona ricorsivamente la directory corrente e tutte le sottodirectory
     for root, _, files in os.walk(os.path.abspath(folder)):
@@ -44,9 +62,11 @@ def verify_files(folder):
                 # Analizza l'output per verificare se la firma è valida
                 if "[GNUPG:] GOODSIG" in result.stdout:
                     print(f"\nSignature is valid for {file_with_asc}")
+                    number_of_signatures_verified += 1
                 else:
                     print(f"\nSignature verification failed for {file_with_asc}")
                     all_signatures_valid = False
+                    number_of_signatures_verified += 1
 
                 print("----------------------------------------------------------------------------------\n")
             except subprocess.CalledProcessError as e:
@@ -54,27 +74,32 @@ def verify_files(folder):
                 print(f"Error verifying {file_with_asc}: {e}")
                 print(f"Command output: {e.stdout}")
                 print(f"Command error: {e.stderr}")
+
     
-    return all_signatures_valid
+    if number_of_signatures_verified == 0:
+        print("Nessun file .asc trovato nella cartella specificata.")
+    else:
+        print("----------------------------------------------------------------------------------\n")
+        if all_signatures_valid:
+            print("Tutte le firme sono valide\n\n")
+        else:
+            print("Non tutte le firme sono valide\n\n")
+
+    
 
 # Controlla se è stato passato l'argomento che indica il percorso della cartella su cui eseguire la verifica delle firme
-if len(sys.argv) != 2:
-    print("Usage: python artifacts_verification_script.py <absolute_path_to_folder>")
+if len(sys.argv) != 3:
+    print("Usage: python artifacts_verification_script.py <absolute_path_to_folder> <absolute_path_to_publickey.asc_file>")
     sys.exit(1)
 
 print("Passato il numero corretto di argomenti\n")
 
-# Ottiene il percorso assoluto dalla riga di comando
+# Ottiene i percorsi assoluti dalla riga di comando
 folder = sys.argv[1]
+publickey = sys.argv[2]
 
-print("Cartella da verificare:", folder)
+print("Cartella da verificare: ", folder)
+print("Path assoluto della public key: ", publickey)
 
 # Esegue la funzione di verifica
-result = verify_files(folder)
-
-print("----------------------------------------------------------------------------------\n")
-# Stampa il risultato finale della verifica
-if result:
-    print("\nTutte le firme sono valide.")
-else:
-    print("\nNon tutte le firme sono valide.")
+verify_files(folder, publickey)
